54518ade618f14881ef1771b3a84d115b25902cd
Renato Covarrubias <rmcovarrubias@falabella.cl>
2019-10-26 19:53:26 -0300

    Apply black

--- pingdom2slack.py
+++ pingdom2slack.py
@@ -11,36 +11,39 @@ import os
 app = Flask(__name__)
 
 try:
-    SLACK_WEBHOOK=os.environ['SLACK_WEBHOOK']
+    SLACK_WEBHOOK = os.environ["SLACK_WEBHOOK"]
     app.logger.debug("Slack webhook URL found in env variable")
 except:
-    SLACK_WEBHOOK=False
+    SLACK_WEBHOOK = False
     app.logger.error("No Slack webhook URL found")
     abort(502)
+
 
 def post_2_slack(channel, check_name, check_url, status, error):
     # Choose attachment colot
     if status == "UP":
-        color="good"
+        color = "good"
     else:
-        color="danger"
+        color = "danger"
 
     # Let's build our payload
     payload = {
-        "channel" : channel,
-        "attachments" : [{
-           "fallback"  	: "%s is %s" % (check_name, status),
-	   "color" 	: color,
-           "mrkdwn_in"  : ["text"],
-           "title"      : "%s is %s" % (check_name, status)
-        }]
+        "channel": channel,
+        "attachments": [
+            {
+                "fallback": "%s is %s" % (check_name, status),
+                "color": color,
+                "mrkdwn_in": ["text"],
+                "title": "%s is %s" % (check_name, status),
+            }
+        ],
     }
 
     if status == "DOWN":
-        payload['attachments'][0]['text'] = "%s | %s" % (check_url, error)
+        payload["attachments"][0]["text"] = "%s | %s" % (check_url, error)
 
     # Add specific headers
-    headers = { "Content-Type": "application/json"}
+    headers = {"Content-Type": "application/json"}
 
     # Make the call
     r = requests.post(SLACK_WEBHOOK, headers=headers, data=json.dumps(payload))
@@ -50,23 +53,25 @@ def post_2_slack(channel, check_name, check_url, status, error):
         app.logger.debug(r.content)
         return "NOK", 400
 
-@app.route("/monitoring/health", methods=['GET'])
+
+@app.route("/monitoring/health", methods=["GET"])
 def health():
     if SLACK_WEBHOOK:
-    	resp = make_response(json.dumps({ "slack_webhook" : True }), 200)
+        resp = make_response(json.dumps({"slack_webhook": True}), 200)
     else:
-    	resp = make_response(json.dumps({ "slack_webhook" : False }), 502) 
-    resp.headers['Content-Type'] = "application/json"
+        resp = make_response(json.dumps({"slack_webhook": False}), 502)
+    resp.headers["Content-Type"] = "application/json"
     return resp
 
-@app.route("/<channel>", methods=['GET', 'POST'])
+
+@app.route("/<channel>", methods=["GET", "POST"])
 def slack_poster(channel):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Initialize the fields of our message
-        check_name 	= None
-        check_url 	= None
-        status 		= None
-        error 		= None
+        check_name = None
+        check_url = None
+        status = None
+        error = None
 
         # Define a default channell
         if not channel:
@@ -79,37 +84,41 @@ def slack_poster(channel):
             app.logger.error(e)
 
         try:
-            check_name = pingdom_data['check_name']
+            check_name = pingdom_data["check_name"]
         except Exception as e:
             app.logger.error("Impossible to extract check_name from this pingdom call")
             app.logger.error(e)
             app.logger.debug(pingdom_data)
 
         try:
-            if 'full_url' in pingdom_data['check_params'].keys():
-                check_url = pingdom_data['check_params']['full_url']
+            if "full_url" in pingdom_data["check_params"].keys():
+                check_url = pingdom_data["check_params"]["full_url"]
             else:
-                check_url = pingdom_data['check_params']['hostname']
+                check_url = pingdom_data["check_params"]["hostname"]
         except Exception as e:
-            app.logger.error("Impossible to extract the full_url from this pingdom call")
+            app.logger.error(
+                "Impossible to extract the full_url from this pingdom call"
+            )
             app.logger.error(e)
             app.logger.debug(pingdom_data)
-        
+
         try:
-            status = pingdom_data['current_state']
+            status = pingdom_data["current_state"]
         except Exception as e:
             app.logger.error("Impossible to extract status from this pingdom call")
             app.logger.error(e)
             app.logger.debug(pingdom_data)
 
         try:
-            error = pingdom_data['long_description']
+            error = pingdom_data["long_description"]
         except Exception as e:
-            app.logger.error("Impossible to extract error message from this pingdom call")
+            app.logger.error(
+                "Impossible to extract error message from this pingdom call"
+            )
             app.logger.error(e)
             app.logger.debug(pingdom_data)
 
         app.logger.debug("Posting to %s: %s is %s" % (channel, check_name, status))
-        return post_2_slack("#%s" % channel,check_name, check_url, status, error)
+        return post_2_slack("#%s" % channel, check_name, check_url, status, error)
     else:
         return "pingdom2slack : pingdom alerts to slack webhkook !"
