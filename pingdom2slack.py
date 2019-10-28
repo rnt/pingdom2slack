80e1bf44edec3544fe792add3b9777f602e8088a
Renato Covarrubias <rmcovarrubias@falabella.cl>
2019-10-28 19:21:05 -0300

    First approximation to full message

--- pingdom2slack.py
+++ pingdom2slack.py
@@ -14,12 +14,70 @@ try:
     SLACK_WEBHOOK = os.environ["SLACK_WEBHOOK"]
     app.logger.debug("Slack webhook URL found in env variable")
 except:
-    SLACK_WEBHOOK = False
+    SLACK_WEBHOOK = None
     app.logger.error("No Slack webhook URL found")
     abort(502)
 
+try:
+    PINGDOM_TOKEN = os.environ["PINGDOM_TOKEN"]
+    app.logger.debug("Pingdom Token found in env variable")
+except:
+    PINGDOM_TOKEN = None
+    app.logger.error("No Pingdom Token found")
+    abort(502)
+
+
+EMOJI_NUMBER = [
+    ":one:",
+    ":two:",
+    ":three:",
+    ":four:",
+    ":five:",
+    ":six:",
+    ":seven:",
+    ":eight:",
+    ":nine:",
+    ":keycap_ten:",
+]
+
+
+def pingdom_analysis(check_id, state_changed_timestamp):
+    # Add specific headers
+    headers = {"Authorization": "Bearer %s" % PINGDOM_TOKEN}
+
+    url = "https://api.pingdom.com/api/3.1/analysis/%d" % check_id
+
+    # Make the call
+    response = requests.get(url, headers=headers)
+    app.logger.debug(response.__dict__)
+    if response.status_code != 200:
+        app.logger.debug(response.content)
+        return None
+
+    analysis = response.json()["analysis"]
+    analysis_ids = [
+        test["id"]
+        for test in analysis
+        if test["timefirsttest"] == state_changed_timestamp
+    ]
+    app.logger.debug("pingdom analysis id = %d" % analysis_ids[0])
+
+    url = "https://api.pingdom.com/api/3.1/analysis/%d/%d" % (check_id, analysis_ids[0])
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        app.logger.debug(response.content)
+        return None
+    else:
+        return response.json()
+
 
 def post_2_slack(channel, pingdom_data):
+
+    analysis = pingdom_analysis(
+        pingdom_data["check_id"], pingdom_data["state_changed_timestamp"]
+    )
 
     check_name = pingdom_data["check_name"]
 
@@ -39,92 +97,132 @@ def post_2_slack(channel, pingdom_data):
         pingdom_data["check_params"]["verify_certificate"], ":thinking_face:"
     )
 
-    fields = [
+    fields = []
+
+    BLOCK_ID_WEBHOOK_DATA = 1
+
+    blocks = [
         {
-            "short": True,
-            "title": "Response Time Threshold",
-            "value": "%d ms" % pingdom_data["check_params"]["responsetime_threshold"],
+            "text": {"text": "*%s* is *%s*." % (check_name, status), "type": "mrkdwn"},
+            "type": "section",
         },
-        {"short": True, "title": "Verify Certificate", "value": verify_certificate},
+        {
+            "fields": [
+                {
+                    "text": "*Response Time Threshold:*\n%s ms"
+                    % pingdom_data["check_params"]["responsetime_threshold"],
+                    "type": "mrkdwn",
+                },
+                {
+                    "text": "*Verify Certificate:*\n%s" % verify_certificate,
+                    "type": "mrkdwn",
+                },
+            ],
+            "type": "section",
+        },
+        {"type": "divider"},
+        {
+            "text": {
+                "text": "Downtime and *Root Cause Analysis* "
+                "(<https://www.pingdom.com/tutorial/downtime-root-cause/|read more>)",
+                "type": "mrkdwn",
+            },
+            "type": "section",
+        },
     ]
 
     if len(pingdom_data["check_params"]["shouldcontain"]) > 0:
-        fields.append(
+        blocks[BLOCK_ID_WEBHOOK_DATA]["fields"].append(
             {
-                "short": True,
-                "title": "Should Contain",
-                "value": "`%s`" % pingdom_data["check_params"]["shouldcontain"],
+                "text": "*Should Contain:*\n`%s`"
+                % pingdom_data["check_params"]["shouldcontain"],
+                "type": "mrkdwn",
             }
         )
 
     if len(pingdom_data["check_params"]["shouldnotcontain"]) > 0:
-        fields.append(
+        blocks[BLOCK_ID_WEBHOOK_DATA]["fields"].append(
             {
-                "short": True,
-                "title": "Should Not Contain",
-                "value": "`%s`" % pingdom_data["check_params"]["shouldnotcontain"],
+                "text": "*Should Not Contain:*\n`%s`"
+                % pingdom_data["check_params"]["shouldnotcontain"],
+                "type": "mrkdwn",
             }
         )
 
     if len(pingdom_data["first_probe"]["location"]) > 0:
-        fields.append(
+        blocks[BLOCK_ID_WEBHOOK_DATA]["fields"].append(
             {
-                "short": True,
-                "title": "First Probe",
-                "value": pingdom_data["first_probe"]["location"],
+                "text": "*First Probe:*\n%s" % pingdom_data["first_probe"]["location"],
+                "type": "mrkdwn",
             }
         )
 
     if len(pingdom_data["second_probe"].get("location", "")) > 0:
-        fields.append(
+        blocks[BLOCK_ID_WEBHOOK_DATA]["fields"].append(
             {
-                "short": True,
-                "title": "Second Probe",
-                "value": pingdom_data["second_probe"]["location"],
+                "text": "*Second Probe:*\n%s"
+                % pingdom_data["second_probe"]["location"],
+                "type": "mrkdwn",
             }
         )
+
+    analysis_counter = 0
+
+    for task in analysis["analysisresult"]["tasks"]:
+
+        blocks.append(
+            {
+                "text": {
+                    "text": "%s analysis" % EMOJI_NUMBER[analysis_counter],
+                    "type": "mrkdwn",
+                },
+                "type": "section",
+            }
+        )
+        fields = []
+        raw_response = None
+
+        for result in task["result"]:
+
+            value = result["value"]
+
+            if result["name"] == "raw_response":
+                # value = "```%s```" % "\n".join(result["value"])
+                raw_response = "```%s```" % "\n".join(result["value"])
+                continue
+            elif result["name"] == "communication_log":
+                continue
+                # if len(result["value"][0]["response_content"]) > 0:
+                #     value = "```%s```\n\n```%s```" % (
+                #         result["value"][0]["request"],
+                #         result["value"][0]["response_content"],
+                #     )
+                # else:
+                #     value = "```%s```" % (result["value"][0]["request"])
+
+            fields.append(
+                {"text": "*%s:*\n%s" % (result["name"], value), "type": "mrkdwn"}
+            )
+
+        blocks.append({"fields": fields, "type": "section"})
+        if raw_response is not None:
+            blocks.append(
+                {
+                    "text": {
+                        "text": "*Raw Response:*\n```%s```" % raw_response,
+                        "type": "mrkdwn",
+                    },
+                    "type": "section",
+                }
+            )
+        blocks.append({"type": "divider"})
+        analysis_counter += 1
 
     # Let's build our payload
     payload = {
         "channel": channel,
-        "blocks": [
-            {
-                "text": {
-                    "text": "*%s* is *%s*." % (check_name, status),
-                    "type": "mrkdwn",
-                },
-                "type": "section",
-            },
-            {
-                "text": {
-                    "text": "%s" % (pingdom_data["long_description"]),
-                    "type": "mrkdwn",
-                },
-                "type": "section",
-            },
-            {
-                "text": {"text": "URL: %s" % (check_url), "type": "mrkdwn"},
-                "type": "section",
-            },
-            {
-                "elements": [
-                    {
-                        "text": "State Changed: %s"
-                        % pingdom_data["state_changed_utc_time"],
-                        "type": "mrkdwn",
-                    }
-                ],
-                "type": "context",
-            },
-        ],
-        "attachments": [
-            {
-                "fallback": "%s is %s" % (check_name, status),
-                "color": color,
-                "mrkdwn_in": ["text"],
-                "fields": fields,
-            }
-        ],
+        "blocks": blocks,
+        "attachments": [],
         "username": "Pingdom",
     }
 
@@ -143,10 +241,20 @@ def post_2_slack(channel, pingdom_data):
 
 @app.route("/monitoring/health", methods=["GET"])
 def health():
-    if SLACK_WEBHOOK:
-        resp = make_response(json.dumps({"slack_webhook": True}), 200)
-    else:
-        resp = make_response(json.dumps({"slack_webhook": False}), 502)
+    status = 200
+    if SLACK_WEBHOOK is None or PINGDOM_TOKEN is None:
+        status = 500
+
+    resp = make_response(
+        json.dumps(
+            {
+                "SLACK_WEBHOOK": SLACK_WEBHOOK is not None,
+                "PINGDOM_TOKEN": PINGDOM_TOKEN is not None,
+            }
+        ),
+        status,
+    )
+
     resp.headers["Content-Type"] = "application/json"
     return resp
 
